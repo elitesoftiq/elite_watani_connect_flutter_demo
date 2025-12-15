501a062c5f8355cb27c76db631415299ffe6df40
Bilal raad <bilal52040@gmail.com>
2025-12-15 16:38:44 +0300

    chore: Set isProduction flag to false

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isProduction = true;
+    bool isProduction = false;
 
     return MaterialApp(
       home: Builder(

98661f1fc69f20d86995924ec060e9665138a5ba
Bilal raad <bilal52040@gmail.com>
2025-12-29 16:35:46 +0300

    fix: Wrap InAppWebView in a SizedBox with infinite height to ensure it fills vertical space.

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -35,38 +35,41 @@ class _WebviewPageState extends State<WebviewPage> {
     return Scaffold(
       appBar: AppBar(title: Text("Payment")),
 
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri(widget.alwataniUrl)),
-        onReceivedServerTrustAuthRequest: (controller, challenge) async {
-          return ServerTrustAuthResponse(
-            action: ServerTrustAuthResponseAction.PROCEED,
-          );
-        },
-        initialSettings: InAppWebViewSettings(
-          transparentBackground: true,
-          useOnDownloadStart: true,
-          horizontalScrollBarEnabled: false,
-          disableHorizontalScroll: true,
-          iframeAllowFullscreen: true,
-          maximumZoomScale: 0,
-          supportZoom: false,
-          preferredContentMode: UserPreferredContentMode.MOBILE,
-          userAgent: kIsWeb
-              ? 'Mozilla/5.0 (Linux; Android 13) '
-                    'AppleWebKit/537.36 (KHTML, like Gecko) '
-                    'Chrome/116.0.5845.163 '
-                    'Mobile Safari/537.36'
-              : Platform.isIOS
-              ? "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) "
-                    "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 "
-                    "Mobile/15E148 Safari/604.1"
-              : 'Mozilla/5.0 (Linux; Android 13) '
-                    'AppleWebKit/537.36 (KHTML, like Gecko) '
-                    'Chrome/116.0.5845.163 '
-                    'Mobile Safari/537.36',
+      body: SizedBox(
+        height: double.infinity,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: WebUri(widget.alwataniUrl)),
+          onReceivedServerTrustAuthRequest: (controller, challenge) async {
+            return ServerTrustAuthResponse(
+              action: ServerTrustAuthResponseAction.PROCEED,
+            );
+          },
+          initialSettings: InAppWebViewSettings(
+            transparentBackground: true,
+            useOnDownloadStart: true,
+            horizontalScrollBarEnabled: false,
+            disableHorizontalScroll: true,
+            iframeAllowFullscreen: true,
+            maximumZoomScale: 0,
+            supportZoom: false,
+            preferredContentMode: UserPreferredContentMode.MOBILE,
+            userAgent: kIsWeb
+                ? 'Mozilla/5.0 (Linux; Android 13) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/116.0.5845.163 '
+                      'Mobile Safari/537.36'
+                : Platform.isIOS
+                ? "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) "
+                      "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 "
+                      "Mobile/15E148 Safari/604.1"
+                : 'Mozilla/5.0 (Linux; Android 13) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/116.0.5845.163 '
+                      'Mobile Safari/537.36',
+          ),
+          onWebViewCreated: (controller) {},
+          shouldOverrideUrlLoading: onRedirection,
         ),
-        onWebViewCreated: (controller) {},
-        shouldOverrideUrlLoading: onRedirection,
       ),
     );
   }
